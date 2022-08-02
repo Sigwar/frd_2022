3a87b8311d2bdda7b646a86c38e96aeb925276ea
rafal.krukowski <1234%^&*qwerTYUI>
2022-08-02 19:57:06 +0200

    feat: Styling custom-input-component, change file extension stories

--- src/components/form/input/input.component.vue
+++ src/components/form/input/input.component.vue
@@ -1,5 +1,5 @@
 <script>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 export default {
   name: "cInput",
   props: {
@@ -9,32 +9,67 @@ export default {
     },
     limit: {
       type: Number,
-      default: 12,
+      default: 10,
     },
-    value: {
+    modelValue: {
       type: String,
       default: "",
     },
+    placeholder: {
+      type: String,
+      default: '',
+    }
   },
+  emits: ['update:modelValue'],
   setup(props) {
+    const input = ref(null);
+    const isFocused = ref(false);
+
     const isTextTooLong = computed(() => {
-      return props.value.length > props.limit
+      return props.modelValue.length > props.limit
     })
 
-    return { isTextTooLong }
+    const focusInput = () => {
+      input.value.focus();
+      isFocused.value = true
+    }
+
+    const isPlaceholderTop = computed(()=> {
+      return props.modelValue !== '' || isFocused.value
+    })
+
+    return {
+      input,
+      isFocused,
+      focusInput,
+      isTextTooLong,
+      isPlaceholderTop
+    }
   }
 };
 </script>
 
 <template>
-  <div class="p-3 rounded-md bg-gray-200 inline-block">
-    <input class="border px-2"
-           :class="isTextTooLong ? 'border-red-500 bg-red-200' : undefined"
-           v-model="value"
-           @input="$emit('input', value)"
-    />
-    <p v-if="isTextTooLong" class="text-red-600 text-xs">
-      Limited to {{ limit }} chars.
-    </p>
+  <div class="p-3 relative">
+
+    <div class="relative">
+      <input ref="input"
+             :class="{'border-rose-500' : isTextTooLong}"
+             :value="modelValue"
+             @input="$emit('update:modelValue', $event.target.value)"
+             @focus="isFocused = true"
+             @focusout="isFocused = false"
+             class="rounded-lg py-4 border-gray-400 focus:border-indigo-600 border-2 bg-transparent px-2 outline-none w-full"/>
+
+      <span v-if="placeholder"
+            @click="focusInput"
+            :class="{'top-0' : isPlaceholderTop }"
+            class="duration-100 p-2 py-1 absolute top-1/2 left-4 -translate-y-1/2 bg-neutral-800 text-neutral-500 font-semibold">{{placeholder}}</span>
+    </div>
+
+    <span v-if="isTextTooLong"
+          class="px-1 w-full text-left inline-block text-rose-500 text-xs">
+      {{ $t('limited to {limit} chars', { limit: limit }) }}
+    </span>
   </div>
 </template>
--- src/views/Home.views.vue
+++ src/views/Home.views.vue
@@ -1,19 +1,28 @@
 <script>
+import { reactive } from 'vue'
 import cInput from '@/components/form/input/input.component.vue'
 
 export default {
   name: "vHome",
   components: {
     cInput
+  },
+  setup() {
+    const myForm = reactive({
+      name: ''
+    })
+
+    return {
+      myForm
+    }
   }
 }
 </script>
 
 <template>
-  <h1>{{ $t('hello') }}</h1>
-
-  <c-input value="test"
-           :limit="3"/>
+  <c-input v-model="myForm.name"
+           :limit="3"
+           placeholder="my-placeholder"/>
 </template>
 
 <style scoped>
