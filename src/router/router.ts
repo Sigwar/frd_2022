5479556bccaa93ab6c250330786030c1ab3382f8
rafal.krukowski <1234%^&*qwerTYUI>
2022-08-25 20:20:48 +0200

    feat: Create Dashboard page

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,17 +1,23 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import vRegister from '@/views/Register/Register.view.vue'
-import vLogin from '@/views/Login/Login.view.vue'
+import vDashboard from '@/views/Dashboard/Dashboard.view.vue'
+import vLogin from '@/views/Login/Login.view.vue';
 
 const routes = [
     {
-        path: '/login',
-        name: 'Login',
-        component: vLogin
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: vDashboard
     },
     {
         path: '/register',
         name: 'Register',
         component: vRegister,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: vLogin
     }
 ]
 
---
+++ src/views/dashboard/Dashboard.view.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "vDashboard"
+}
+</script>
+
+<template>
+<div>
+  ASD
+</div>
+</template>
+
+<style lang="scss"
+       scoped>
+
+</style>
